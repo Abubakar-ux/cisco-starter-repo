a0979c9fe2b03050af63c9f03e52c033b622b876
Aroshia <swiftboy432@gmail.com>
2022-07-15 01:43:19 +0500

    All Tasks done

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -3,17 +3,21 @@ import './App.css';
 import Banner from './Banner';
 import Exhibit from './Exhibit';
 import AddressDisplay from './AddressDisplay';
+import PylonConnector from './PylonConnector';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <Banner bannerText="Sextant" />
-                <Exhibit name="Public IPv4 Address">
+                <Banner bannerText="Ahmed's Sextant" />
+                <Exhibit name="IPv4 Address">
                     <AddressDisplay url='https://api.ipify.org?format=json' />
                 </Exhibit>
-                <Exhibit name="Public IPv6 Address">
+                <Exhibit name="IPv6 Address">
                     <AddressDisplay url='https://api64.ipify.org?format=json' />
+                </Exhibit>
+                <Exhibit name="Pylon Packet Latency">
+                    <PylonConnector />
                 </Exhibit>
             </div>
         );
